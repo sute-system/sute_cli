6edd4b950d7d857c326012e023fe694608377ca4
cxw <1660509931@qq.com>
2022-12-22 14:22:03 +0800

    fix()功能开发

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const program = require("commander");
+const helpOptions = require("./lib/core/help.js");
+const createCommands = require("./lib/core/create")
+
+// 查看版本号
+program.version(require("./package.json").version);
+
+// 帮助和可选的信息
+helpOptions();
+
+// 创建其他指令-执行对应命令
+createCommands()
+
+
+
+program.parse(program.argv);
